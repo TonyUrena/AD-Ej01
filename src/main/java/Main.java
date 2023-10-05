9bebfe687c64396bfe0d962019aff35ff1adfd2f
TonyUre <aurenfort@gmail.com>
2023-10-05 18:28:32 +0200

    ejercicio entregado

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Path archivo = Path.of("C:/tmp/niats");
+
+        try (Stream<Path> fileList = Files.walk(archivo).skip(1).sorted(Comparator.comparing(Path::getNameCount).reversed())){
+            fileList.forEach((p) -> {
+                try {
+                    if (Files.isRegularFile(p)){
+                        Files.move(p, Path.of(archivo.toAbsolutePath() + "/" + p.getFileName()));
+                    } else {
+                        Files.delete(p);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
